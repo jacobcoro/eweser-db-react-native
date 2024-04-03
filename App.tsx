8226eeeb4e462d6a34d8cf2e2d5ba1f47fffba15
jacobcoro <xiaokourou@gmail.com>
2024-04-03 17:02:14 +0800

    set up web

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+// @refresh resetnpc
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Database } from '@eweser/db';
+
 export default function App() {
+  const db = new Database();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{db.authServer}</Text>
       <StatusBar style="auto" />
     </View>
   );
